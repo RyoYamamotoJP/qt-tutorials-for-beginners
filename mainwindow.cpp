68360a89de22465f58b757ef18ac627e1e4047ad
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-05-31 09:17:36 +0900

    Add image to main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("/Users/ryo/Downloads/qt.png");
+    int w = ui->label_pic->width();
+    int h = ui->label_pic->height();
+    ui->label_pic->setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
