c0dca6ef772634fba42de1c21d8591c329906d37
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-05-31 09:33:01 +0900

    Show message in status bar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,6 +12,8 @@ MainWindow::MainWindow(QWidget *parent) :
     int w = ui->label_pic->width();
     int h = ui->label_pic->height();
     ui->label_pic->setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
+    ui->statusBar->addPermanentWidget(ui->label_3);
+    ui->statusBar->addPermanentWidget(ui->progressBar);
 }
 
 MainWindow::~MainWindow()
@@ -25,11 +27,15 @@ void MainWindow::on_pushButton_login_clicked()
     QString password = ui->lineEdit_password->text();
 
     if (username == "test" && password == "test") {
-        QMessageBox::information(this, "Login", "Username and password is correct");
-        hide();
-        secDialog = new SecDialog(this);
-        secDialog->show();
+//        QMessageBox::information(this, "Login", "Username and password is correct");
+//        hide();
+//        secDialog = new SecDialog(this);
+//        secDialog->show();
+        ui->statusBar->showMessage("Username and password is correct", 5000);
+        ui->label_3->setText("Username and password is correct");
     } else {
-        QMessageBox::warning(this, "Login", "Username and password is not correct");
+//        QMessageBox::warning(this, "Login", "Username and password is not correct");
+        ui->statusBar->showMessage("Username and password is not correct", 5000);
+        ui->label_3->setText("Username and password is not correct");
     }
 }
