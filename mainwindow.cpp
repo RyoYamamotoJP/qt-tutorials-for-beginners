51b429700ca7712bfd38aacdab3ec297d87b0f7b
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-05-31 13:46:13 +0900

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    if (ui->radioButton->isChecked()) {
+        QMessageBox::information(this, "title", "Mark");
+    }
+    if (ui->radioButton_2->isChecked()) {
+        QMessageBox::information(this, "title", "John");
+    }
+}
