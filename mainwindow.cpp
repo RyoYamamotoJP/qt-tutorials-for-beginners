c943facbda7745dcaa9f2e8bbef45a4b7c2f0196
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-06-01 16:35:10 +0900

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,105 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    file_path = "";
+    ui->textEdit->setText("");
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString file_name = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(file_name);
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "..", "File not open");
+        return;
+    }
+    file_path = file_name;
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    QFile file(file_path);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "..", "File not open");
+        return;
+    }
+    QTextStream out(&file);
+
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    out.flush();
+    file.close();
+}
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    QString file_name = QFileDialog::getSaveFileName(this, "Open the file");
+    QFile file(file_name);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "..", "File not open");
+        return;
+    }
+    file_path = file_name;
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    out.flush();
+    file.close();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionAbout_Notepad_triggered()
+{
+    QString about_text;
+    about_text = "Auther: somebody\n";
+    about_text += "Date : 02/06/2019\n";
+    about_text += "(C) Notepad(R)\n";
+    QMessageBox::about(this, "About Notepad", about_text);
+}
