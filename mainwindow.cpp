454254b974bff5262e86e821b9eeade8170166b4
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-06-01 11:48:51 +0900

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QFile file("/Users/ryo/work/QFileDemo/myfile.txt");
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "title", "file not open");
+    }
+    QTextStream out(&file);
+    QString text = ui->plainTextEdit->toPlainText();
+    out << text;
+    file.flush();
+    file.close();
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QFile file("/Users/ryo/work/QFileDemo/myfile.txt");
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "title", "file not open");
+    }
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->plainTextEdit->setPlainText(text);
+    file.close();
+}
